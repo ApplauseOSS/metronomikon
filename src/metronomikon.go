997288e9b80ab8840d502622f740a7a438315d6c
Ales Verbic <averbic@applause.com>
2024-04-29 14:51:13 -0400

    feat: add log/slog
    Signed-off-by: Ales Verbic <averbic@applause.com>

--- src/metronomikon.go
+++ src/metronomikon.go
@@ -3,11 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log/slog"
+	"os"
+
 	"github.com/applauseoss/metronomikon/api"
 	"github.com/applauseoss/metronomikon/config"
 	"github.com/applauseoss/metronomikon/kube"
-	"log"
-	"os"
 )
 
 var defaultConfigFile = "/etc/metronomikon/config.yaml"
@@ -18,6 +19,15 @@ func main() {
 	flag.BoolVar(&debug, "debug", false, "Enable debug mode")
 	flag.StringVar(&configFile, "config", "", fmt.Sprintf("Path to config file (defaults to %s, if it exists)", defaultConfigFile))
 	flag.Parse()
+	// Setup logger
+	logLevel := slog.LevelInfo
+	if debug {
+		logLevel = slog.LevelDebug
+	}
+	logger := slog.New(
+		slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: logLevel}),
+	)
+	slog.SetDefault(logger)
 	// Use default config file path if none was provided and it exists
 	if configFile == "" {
 		if _, err := os.Stat(defaultConfigFile); err == nil {
@@ -27,14 +37,16 @@ func main() {
 	// Load config file if specified
 	if configFile != "" {
 		if err := config.LoadConfig(configFile); err != nil {
-			log.Fatalf("Failed to load config file: %s", err)
+			slog.Error(fmt.Sprintf("Failed to load config file: %s", err))
+			os.Exit(1)
 		}
 	}
 	if err := kube.TestClientConnection(); err != nil {
-		log.Fatalf("Failed to connect to Kubernetes API: %s", err)
+		slog.Error(fmt.Sprintf("Failed to connect to Kubernetes API: %s", err))
+		os.Exit(1)
 	} else {
 		if debug {
-			log.Print("Successfully initialized kubernetes client")
+			slog.Info("Successfully initialized kubernetes client")
 		}
 	}
 	a := api.New(debug)
